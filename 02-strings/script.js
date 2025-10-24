549ba43e7661a67a06cdcf7587401279b4c731e3
leopii98 <rippergames98@gmail.com>
2025-10-24 17:26:26 +0200

    strings-done

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,14 +1,20 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-const nomeProp = Leo
-const cogNome = Palmieri
-const nomePropPluscogNome = nomeProp + cogNome;
-console.log(nomePropPluscogNome); 
+const myName = 'Leo';
+console.log(myName); // Output: Leo
+
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
+const myText = 'Text';
+const numberTwo = 2
+const booleanValor = true
+
+console.log(myText); //Output: Text
+console.log(numberTwo); //Output: 2
+console.log(booleanValor); //Output: true
 
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
@@ -17,10 +23,10 @@ const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // NON MODIFICARE QUESTA RIGA
 const firstName = 'Jane'; // NON MODIFICARE QUESTA RIGA
 const lastName = 'Smith'; // NON MODIFICARE QUESTA RIGA
 
-const fullName = null;
+const fullName = firstName + " " + lastName;
 console.log(fullName); // Output: "Jane Smith"
 
 // 4. Imposta questa variabile utilizzando una proprietà
 // sulla variabile alphabet per ottenere la sua lunghezza
-const numberOfLettersInAlphabet = null;
+const numberOfLettersInAlphabet = alphabet.length;
 console.log(numberOfLettersInAlphabet); // Output: 26
